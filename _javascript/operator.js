126b044f91841c405b1d8315af538b0a1b9611e3
Zac <a19860101@gmail.com>
2021-11-11 21:39:26 +0800

    20211111

--- _javascript/operator.js
+++ _javascript/operator.js
@@ -25,3 +25,17 @@ let a = 10;
 let b = '10';
 console.log(a == b);
 console.log(a === b);//值與資料型態皆須相等
+
+// 指定運算子 //賦值運算子
+
+// console.log(x = y);
+// console.log(x += y);//x = x + y
+// console.log(x -= y);//x = x - y
+// console.log(x *= y);//x = x * y
+// console.log(x /= y);//x = x / y
+// console.log(x %= y);//x = x % y
+
+// 邏輯運算子 && || !
+console.log(x > 0 && y < 0);
+console.log(x > 0 || y < 0);
+console.log(!x);
