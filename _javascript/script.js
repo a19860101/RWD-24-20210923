ce2c57a403e82debb37c1837b85b7f27ee9fd4c4
Zac <a19860101@gmail.com>
2021-11-11 20:36:36 +0800

    20211111

--- _javascript/script.js
+++ _javascript/script.js
@@ -1,1 +1,18 @@
-alert('Hello 我是外部');
+// alert('Hello 我是外部');
+
+// 變數
+
+var a = 10;
+var color = 'red';
+
+let b = 99;
+let text = 'hello js';
+
+/* 
+    變數命名規範
+    首字不可使用特殊符號與數字，除了底線
+    大小寫有別
+    不可使用保留字
+*/
+
+console.log(text);
