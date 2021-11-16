de20b765c9996f84b67b05404420cf3e5fc7aa6c
Zac <a19860101@gmail.com>
2021-11-16 21:39:12 +0800

    20211116

--- _javascript/array-2.js
+++ _javascript/array-2.js
@@ -3,6 +3,35 @@ drinks.push('拿鐵');
 
 // console.log(drinks);
 
+// foreach
 drinks.forEach(function(drink,idx){
     console.log(drink,idx);
 })
+
+// map
+
+const scores = [80,66,92,75,55,68];
+const scores_map = scores.map(function(data){
+    if(data >= 60){
+        return '及格'
+    }else{
+        return '不及格';
+    }
+})
+
+// console.log(scores_map);
+
+// filter
+const scores_filter = scores.filter(function(data){
+    if(data >= 60){
+        return data;
+    }
+})
+
+console.log(scores_filter);
+
+const drinks_filter = drinks.filter(function(data){
+    return data != '綠茶'
+})
+
+console.log(drinks_filter);
