ce25639c46f2ada9cf3f44402e74db590cb73ad9
Zac <a19860101@gmail.com>
2021-11-16 21:54:00 +0800

    20211116

--- _javascript/array-2.js
+++ _javascript/array-2.js
@@ -1,7 +1,7 @@
 const drinks = ['紅茶','綠茶','奶茶'];
 drinks.push('拿鐵');
 
-// console.log(drinks);
+console.log(drinks);
 
 // foreach
 drinks.forEach(function(drink,idx){
@@ -10,7 +10,7 @@ drinks.forEach(function(drink,idx){
 
 // map
 
-const scores = [80,66,92,75,55,68];
+const scores = [66,80,92,75,55,68];
 const scores_map = scores.map(function(data){
     if(data >= 60){
         return '及格'
@@ -35,3 +35,27 @@ const drinks_filter = drinks.filter(function(data){
 })
 
 console.log(drinks_filter);
+
+//find (無論結果有幾個值，最終只會有一個值被回傳)
+
+const drinks_find = drinks.find(function(data){
+    return data == '拿鐵';
+})
+console.log(drinks_find);
+
+const scores_find = scores.find(function(data){
+    return data >= 60;
+})
+console.log(scores_find);
+
+
+//findIndex (無論結果有幾個值，最終只會有一個值被回傳)
+const drinks_findIndex = drinks.findIndex(function(data){
+    return data == '拿鐵';
+})
+console.log(drinks_findIndex);
+
+const scores_findIndex = scores.findIndex(function(data){
+    return data >= 80;
+})
+console.log(scores_findIndex);
