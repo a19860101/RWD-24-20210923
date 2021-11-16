4fa56e278620d980f66cf0811036e200d6040cc1
Zac <a19860101@gmail.com>
2021-11-16 19:18:25 +0800

    20211116

--- _javascript/condition.js
+++ _javascript/condition.js
@@ -30,3 +30,33 @@ if(x > 100){
 }else{
     console.log('error3');
 }
+
+// switch
+
+let i = 'asdf';
+switch(i){
+    case 0:
+        console.log('星期日');
+        break;
+    case 1:
+        console.log('星期一');
+        break;
+    case 2:
+        console.log('星期二');
+        break;
+    case 3:
+        console.log('星期三');
+        break;
+    case 4:
+        console.log('星期四');
+        break;
+    case 5:
+        console.log('星期五');
+        break;
+    case 6:
+        console.log('星期六');
+        break;
+    default:
+        console.log('請輸入正確的數值');
+
+}
