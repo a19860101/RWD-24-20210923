9da3b897259925e7dc62faa019cd7f26a2580484
Zac <a19860101@gmail.com>
2021-11-16 19:08:54 +0800

    20211116

---
+++ _javascript/condition.js
@@ -0,0 +1,32 @@
+// if
+
+let x = 0;
+
+if( x > 0 ){
+    console.log('success');
+}
+
+// if ... else
+
+if(x > 0){
+    console.log('success2');
+}else{
+    console.log('error2');
+}
+
+// if ... else if
+
+if(x > 100){
+    console.log('>100');
+}else if(x > 0){
+    console.log('>0');
+}
+
+// if ... else if ... else
+if(x > 100){
+    console.log('>100');
+}else if(x > 0){
+    console.log('>0');
+}else{
+    console.log('error3');
+}
