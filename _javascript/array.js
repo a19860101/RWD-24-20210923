fcf8b9ce3c45552f428e7d90d09901e86a38ef23
Zac <a19860101@gmail.com>
2021-11-16 19:56:37 +0800

    20211116

---
+++ _javascript/array.js
@@ -0,0 +1,13 @@
+// let a = new Array();
+// a[0] = 'Apple';
+// a[1] = 'Banana';
+// a[2] = 'Cat';
+
+// let a =  new Array('Apple','Banana','Cat');
+
+let a = ['Apple','Banana','Cat'];
+
+console.log(a);
+console.log(a[0]);
+console.log(a[1]);
+console.log(a[2]);
