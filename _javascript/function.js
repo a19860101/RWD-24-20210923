3d4e7e6c498b88d19d12b0ab5aaeb2faec0d54f5
Zac <a19860101@gmail.com>
2021-11-18 19:19:08 +0800

    20211118

--- _javascript/function.js
+++ _javascript/function.js
@@ -1,12 +1,31 @@
-// 函式陳述式 function statement
+// test();
+// 函式陳述式 function statement (提升性)
 function test(){
     alert('hello test');
 }
+
+
 
 
 // 函式表達式 function expression
 const test2 = function(){
     alert('hello test2');
 }
+// test2()
 
-test2();
+function calc(x){
+    return x;
+}
+
+function square(w,h){
+    let val = w * h;
+    return val;
+}
+
+console.log(square(4,3));
+console.log(square(5,8));
+
+const total = function(price,tax){
+    return price * tax;
+}
+console.log(total(1000,1.5));
